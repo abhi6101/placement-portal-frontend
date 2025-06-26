f0aaf77a56143e75a79309d9f1ae176782fa9947
abhi6101 <abhijain6101@gmail.com>
2025-06-27 00:05:13 +0530

    i am adding a fevicon

---
+++ login frontend 23 4/papers-data.js
@@ -0,0 +1,9 @@
+// papers-data.js
+
+const fileData = [
+    // Account
+    { name: "account_2018_November.pdf", url: "https://drive.google.com/file/d/1--q6B-0SxOmNI1GqQY2LVF-hPEaF7YeU/view?usp=sharing" },
+    { name: "account_2018.pdf", url: "https://drive.google.com/file/d/1-7hbGf6yI8tvSKA_gNfPtw8Q8zzjS3O9/view?usp=sharing" },
+    // ... paste your ENTIRE fileData array here ...
+    { name: "web_technology_November_2023.pdf", url: "https://drive.google.com/file/d/1j-r7S8T9c1d3eG2hI1jK2lM3nO4pQ5R/view?usp=sharing" },
+];
